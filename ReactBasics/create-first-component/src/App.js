3b85a5d7dfea9a2bbd4aa153fd5e4cf8f498099b
Alejandro Chavez <achavez5@wisc.edu>
2023-11-04 16:27:31 -0500

    Lab 1 - Create first component
    Bringing it closer to the desired approach

--- ReactBasics/create-first-component/src/App.js
+++ ReactBasics/create-first-component/src/App.js
@@ -1,19 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function Heading () {
-  return (
-    <h1>This is an h1 heading!</h1>
-  );
+  return ( <h1>This is an h1 heading!</h1> );
 }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header"> */}
         <h1>Hello to the world!</h1>
-        <Heading />
-      </header>
+      {/* </header> */}
+      <Heading />
     </div>
   );
 }
