6c9c83799d6a7b12b36c516233c3379544a48d99
Alejandro Chavez <achavez5@wisc.edu>
2023-11-06 08:04:05 -0600

    Lab | Multiple components - final commit

--- ReactBasics/multiple-components/src/App.js
+++ ReactBasics/multiple-components/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './Cards';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Task: Add three Card elements</h1>
+      <Card h2="First card's h2" h3="First card's h3" />
+      <Card h2="Second card's h2" h3="Second card's h3" />
+      <Card h2="Third card's h2" h3="Third card's h3" />
     </div>
   );
 }
