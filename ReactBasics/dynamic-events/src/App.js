6ca60ac2735e932c4d55963902f2fc4312cde9d9
Alejandro Chavez <achavez5@wisc.edu>
2023-11-06 08:14:43 -0600

    Lab | Dynamic events - final commit

--- ReactBasics/dynamic-events/src/App.js
+++ ReactBasics/dynamic-events/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    let randomNum = Math.floor(Math.random() * 3) + 1; 
+    console.log(randomNum);
+    let userInput = prompt('type a number');
+    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Task: Add a button and handle a click event</h1>
+      <button onClick={handleClick}>Guess the number between 1 and 3</button>
     </div>
   );
 }
