48471c8d492286febba84f48ae7cf1aafab14fa4
Alejandro Chavez <achavez5@wisc.edu>
2023-11-08 21:04:02 -0600

    Lab | Displaying images - final commit

--- ReactBasics/displaying-images-lab/src/App.js
+++ ReactBasics/displaying-images-lab/src/App.js
@@ -1,7 +1,14 @@
+import image from "./assets/images/logo.png"
+
 function App() {
   return (
     <div className="App">
-      
+      <h1>Displaying images lab</h1>
+      <img
+        height={200}
+        src={image}
+        alt="Logo"
+      ></img>
     </div>
   );
 }
