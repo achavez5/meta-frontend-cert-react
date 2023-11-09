fc0a3fce38fb01d27eecab2431d5d58f4615827b
Alejandro Chavez <achavez5@wisc.edu>
2023-11-08 21:21:10 -0600

    Reading | Media packages - using ReactPlayer to play video

---
+++ ReactBasics/media-packages/src/App.js
@@ -0,0 +1,12 @@
+import ReactPlayer from "react-player/youtube"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ReactPlayer url='https://www.youtube.com/watch?v=5LcOOWk9xVE&t=1421s&ab_channel=CalebHammer' />
+    </div>
+  );
+}
+
+export default App;
