11a68a22769e1fed3ddcea8ca43a181be69e98fa
Alejandro Chavez <achavez5@wisc.edu>
2023-11-09 20:24:33 -0600

    Assessment | Calculator app - final commit

--- ReactBasics/calculator-app-assessment/src/App.js
+++ ReactBasics/calculator-app-assessment/src/App.js
@@ -11,27 +11,32 @@ function App() {
  
   function plus(e) { 
     e.preventDefault(); 
-    setResult((result) => result + Number(inputRef.current.value)); 
+    setResult((result) => result + Number(inputRef.current.value) || 0); 
   }; 
  
   function minus(e) { 
-  	// Add the code for the minus function 
+  	e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value) || 0);
   };
  
   function times(e) { 
-    // Add the code for the plus function 
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value) || 0);
   }; 
  
   function divide(e) { 
-    // Add the code for the divide function 
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value) || 0);
   };
  
   function resetInput(e) { 
-    // Add the code for the resetInput function 
+    e.preventDefault();
+    inputRef.current.value = "";
   }; 
  
   function resetResult(e) { 
-  	// Add the code for the resetResult function 
+  	e.preventDefault();
+    setResult((result) => 0);
   }; 
  
   return ( 
@@ -41,7 +46,7 @@ function App() {
       </div> 
       <form> 
         <p ref={resultRef}> 
-          {/* add the value of the current total */} 
+          {result} 
         </p> 
         <input
           pattern="[0-9]" 
@@ -50,11 +55,12 @@ function App() {
           placeholder="Type a number" 
         /> 
         <button onClick={plus}>add</button> 
-        {/* Add the subtract button */} 
-        {/* Add the multiply button */} 
-        {/* Add the divide button */} 
-        {/* Add the resetInput button */} 
-        {/* Add the resetResult button */} 
+        <button onClick={minus}>subtract</button> 
+        <button onClick={times}>multiply</button> 
+        <button onClick={divide}>divide</button> 
+        <button onClick={resetInput}>reset input</button> 
+        <button onClick={resetResult}>reset result</button> 
+
       </form> 
     </div> 
   ); 
