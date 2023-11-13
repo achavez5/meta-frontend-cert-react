4a45acb270291a430bd035121355a66e9ed2e25a
Alejandro Chavez <achavez5@wisc.edu>
2023-11-12 21:02:11 -0600

    Lab | Light-Dark Switcher - final commit

--- AdvancedReact/light-dark-switcher/src/ThemeContext.js
+++ AdvancedReact/light-dark-switcher/src/ThemeContext.js
@@ -1,3 +1,19 @@
-export const ThemeProvider = ({ children }) => children;
+import { createContext, useContext, useState } from "react";
 
-export const useTheme = () => ({ theme: "light" });
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState("light");
+
+    return (
+        <ThemeContext.Provider value={{
+            theme,
+            toggleTheme: () => setTheme(theme === "light" ? "dark" : "light")
+        }}>
+            {children}
+        </ThemeContext.Provider>
+        
+    );
+};
+
+export const useTheme = () => useContext(ThemeContext);
