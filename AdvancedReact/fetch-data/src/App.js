3a3e04d6a712af36c9a2c66bcd6060bcd08d604d
Alejandro Chavez <achavez5@wisc.edu>
2023-11-13 20:57:43 -0600

    Lab | Fetching data - final commit
    Not tested due to timeout on the fetch. Using solution still failed so reverted back to my own code (not very different as I really only added a catch to the promise)

--- AdvancedReact/fetch-data/src/App.js
+++ AdvancedReact/fetch-data/src/App.js
@@ -4,7 +4,14 @@ function App() {
   const [user, setUser] = React.useState([]);
 
   const fetchData = () => {
-
+    fetch("https://randomuser.me/api/?results=1")
+    .then( response => response.json())
+    .then( data => {
+      setUser(data);
+    })
+    .catch((e) => {
+      console.log("Something funky happened:", e) 
+    });
   };
 
   React.useEffect(() => {
@@ -14,7 +21,8 @@ function App() {
   return Object.keys(user).length > 0 ? (
     <div style={{padding: "40px"}}>
       <h1>Customer data</h1>
-
+      <h2>Name: {user.results[0].name.first}</h2>
+      <img src={user.results[0].picture.large} alt="picture of random user" />
     </div>
   ) : (
     <h1>Data pending...</h1>
