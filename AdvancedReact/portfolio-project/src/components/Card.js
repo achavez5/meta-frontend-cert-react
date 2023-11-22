8219c7e986df5ddb36cc75b3a07f0e409dc3674b
Alejandro Chavez <achavez5@wisc.edu>
2023-11-21 21:00:28 -0600

    projects section

--- AdvancedReact/portfolio-project/src/components/Card.js
+++ AdvancedReact/portfolio-project/src/components/Card.js
@@ -7,7 +7,22 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+
+  let borderRadius = "10px";
+
+  return (
+    <div style={ { borderRadius: borderRadius, backgroundColor:"white"}  }>
+      <Image src={imageSrc} alt={description} style={{borderRadius: borderRadius}}/>
+      <VStack space="8px" style={{ padding: "2em", paddingBottom: "0px"}} alignItems="left">
+        <Heading size="md" style={{ color: "black"}}>{title}</Heading>
+        <Text textColor="darkgrey">{description}</Text>
+      </VStack>
+      <HStack space="8px" align-items="left" style={{ padding: "2em" }} color="darkslategrey">
+          <Text><a href="/">See more</a></Text>
+          <FontAwesomeIcon icon={faArrowRight} size="1x" />
+      </HStack>
+    </div>
+  );
 };
 
 export default Card;
